43e4addff282386cc3609c67225f037d4361ce97
Yago Luiz <yago.luiz@hotmail.com>
2020-08-13 14:49:21 -0300

    Add kubernetes and minikube

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -205,6 +205,18 @@ echo "What is your root user for remove 'sudo' docker?"
 read docker_user
 sudo usermod -aG docker $docker_user
 
+echo 'installing kubernetes'
+sudo curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+echo 'installing minikube'
+sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+sudo mkdir -p /usr/local/bin/
+sudo install minikube /usr/local/bin/
+
 echo 'cloud'
 
 echo "AWS developer? (y/n)"
