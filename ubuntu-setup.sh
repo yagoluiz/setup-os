e01a39d3f753da66d60e3810ba30a5cc564b7e5a
Yago Luiz <yago.luiz@hotmail.com>
2021-05-09 18:34:54 -0300

    Add azure functions

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -222,6 +222,14 @@ if echo "$developer_azure" | grep -iq "^y" ;then
     echo "installing azure-cli"
     curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
     az --version
+
+    echo "installing azure functions"
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+    
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
+    
+    sudo apt-get install azure-functions-core-tools-3
 else
 	echo "Okay, no problem. :) Let's move on!"
 fi
