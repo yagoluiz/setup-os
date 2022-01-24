f1a2d56821b9c4f599f9500b2696a6e4ea98c85e
Yago Luiz <yago.luiz@hotmail.com>
2022-01-24 12:58:24 -0300

    Update docker

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -191,14 +191,16 @@ echo "installing postman"
 sudo snap install postman
 
 echo "installing docker" 
-sudo apt-get remove docker docker-engine docker.io
-sudo apt install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
-docker --version
+sudo apt-get remove docker docker-engine docker.io containerd runc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-chmod 777 /var/run/docker.sock
-docker run hello-world
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker
 
 echo "installing docker-compose" 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
