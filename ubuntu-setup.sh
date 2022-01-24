b286b83b3914fce5fbf2898bbefb0ff1130c1ba1
Yago Luiz <yago.luiz@hotmail.com>
2022-01-24 12:59:03 -0300

    Add docker image example

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -202,6 +202,8 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo systemctl enable docker
 
+sudo docker run hello-world
+
 echo "installing docker-compose" 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
