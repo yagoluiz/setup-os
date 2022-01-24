8f8de0a9d7b8f675fd116a4aff297e6211bc6dc8
Yago Luiz <yago.luiz@hotmail.com>
2022-01-24 13:00:26 -0300

    Update docker-compose

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -205,8 +205,9 @@ sudo systemctl enable docker
 sudo docker run hello-world
 
 echo "installing docker-compose" 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 
 echo "What is your root user for remove "sudo" docker?"
